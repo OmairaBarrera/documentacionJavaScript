5208a18b2586a452c2ddeff0870c1d0e84accae8
Luis Rueda <lucho.lgrg04@gmail.com>
2023-03-14 09:00:32 -0500

    feat: :sparkles: Add Declaratives Funtions
    Allow create a function that exist throughout the code.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-/*
-
+/* Funciones por declaración
+permite declarar una función que existirá a lo largo de todo el código
+Incluso antes de haberla creado y funcionaría correctamente
 */
 
+console.log(calcular(5, 8));
 
+function saludar() {
+    return "Hola";
+}
+function calcular(n1 , n2) {
+    return n1*n2;
+}
+
+console.log(saludar());
+typeof saludar; 
 
 
 
