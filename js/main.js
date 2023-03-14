980fe3df5e0f600b5bb8550601d80813ae0a9357
Luis Rueda <lucho.lgrg04@gmail.com>
2023-03-14 08:48:02 -0500

    feat: :rocket: config workspace
    Start with Funtions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-/*Metodos Math.round
-    retorna el valor de un número redondeado al entero más cercano
+/*
+
 */
-console.log("Valor redondeado de varios numeros");
-console.log("Numero mas cercano a .95 = ", Math.round(.95));
-console.log("Numero mas cercano a 4.5 = ", Math.round(4.5)); 
-console.log("Numero mas cercano a 7.004 = ", Math.round(7.004));
-console.log("Numero mas cercano a -7.004 = ", Math.round(-7.004));
+
 
 
 
